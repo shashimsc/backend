3d741543ae43d04311d70bfe9b5647f0b9739a64
shashimsc <shashimsc@gmail.com>
2020-07-24 17:01:56 +0530

    Update question.js

--- models/question.js
+++ models/question.js
@@ -4,7 +4,11 @@ var Question = mongoose.model('Question', {
     question_id: { type: Number },
     question: { type: String },
     q_type: { type: String },
-    participant_type: { type: String }
+    participant_type: { type: String },
+    answer1: { type: String },
+    answer2: { type: String },
+    answer3: { type: String },
+    answer4: { type: String }
 });
 
 module.exports = { Question };
