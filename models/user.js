761387309ba11f693f52ae8f5f595ed77a90d032
shashimsc <shashimsc@gmail.com>
2020-07-30 13:20:41 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,11 +1,52 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-var User = mongoose.model('User', {
-    emp_id: { type: String },
-    name: { type: String },
-    password: { type: String },
-    role: { type: String },
-    active: { type: String}
+var userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'Full name can\'t be empty'
+    },
+    email: {
+        type: String,
+        required: 'Email can\'t be empty',
+        unique: true
+    },
+    password: {
+        type: String,
+        required: 'Password can\'t be empty',
+        minlength : [4,'Password must be atleast 4 character long']
+    },
+     emp_id: {
+        type: String,
+        required: 'EMP ID can\'t be empty'
+    },
+    
+    role: {
+        type: String
+    },
+    
+    active: {
+        type: String
+    },
+    
+    saltSecret: String
 });
 
-module.exports = { User };
+// Custom validation for email
+userSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail.');
+
+// Events
+userSchema.pre('save', function (next) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            this.password = hash;
+            this.saltSecret = salt;
+            next();
+        });
+    });
+});
+
+mongoose.model('User', userSchema);
