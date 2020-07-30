3b9a6472f180af9d4b43175283ea0cbeb78b7bf2
shashimsc <shashimsc@gmail.com>
2020-07-30 13:23:38 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,1 +1,7 @@
-//TODO - add configuratino using config.json file
+// check env.
+var env = process.env.NODE_ENV || 'dev';
+// fetch env. config
+var config = require('./config.json');
+var envConfig = config[env];
+// add env. config values to process.env
+Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
